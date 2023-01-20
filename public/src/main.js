7d5f437b8e765d7a1bc5f5a39834c21956475846
Ph4re <123106362+Ph4re@users.noreply.github.com>
2023-01-20 07:46:31 +0100

    change directory

--- public/src/main.js
+++
@@ -1,5 +0,0 @@
-import Blink from './blink.js';
-
-const trigger = document.getElementById('trigger');
-const popper = document.getElementById('popper');
-Blink.create(trigger, popper);
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import Blink from './blink.js';
+
+const trigger = document.getElementById('trigger');
+const popper = document.getElementById('popper');
+Blink.create(trigger, popper);
